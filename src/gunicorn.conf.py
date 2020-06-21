0bb96cda65ec060762242e7625f390b6bb77ad41
Michael Molino <creative@michaelmolino.com>
2020-06-21 10:15:01 +0100

    add configuration options for ssl key/cert

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -1,7 +1,9 @@
+import os
+
 bind = '0.0.0.0:4444'
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
 workers = 1
 keep_alive = 5
-certfile = '/config/certs/nginx-selfsigned.crt'
-keyfile = '/config/certs/nginx-selfsigned.key'
+certfile = os.environ.get("CERT_FILE") or '/ssl/public.crt'
+keyfile = os.environ.get("KEY_FILE") or '/ssl/private.key'
