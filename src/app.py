191388b296a879f8b74d1e8608e703fb194bb311
Michael Molino <creative@michaelmolino.com>
2020-06-24 23:22:08 +0100

    sonar code smells

--- src/app.py
+++ src/app.py
@@ -23,17 +23,15 @@ app = Flask(__name__, static_folder='static')
 
 @app.route("/oauth2/auth", methods=['GET'])
 def auth():
-    # response_type = request.args.get('response_type')
-    # client_id = request.args.get('client_id')
     redirect_uri = request.args.get('redirect_uri')
     state = request.args.get('state')
-    code = ''.join((random.choice(string.ascii_letters + string.digits) for i in range(32)))
+    code = ''.join((random.choice(string.ascii_letters + string.digits) for _ in range(32)))
     return redirect(redirect_uri + '?code=' + code + '&state=' + state)
 
 
 @app.route("/oauth2/token", methods=['POST'])
 def token():
-    access_token = ''.join((random.choice(string.ascii_letters + string.digits) for i in range(32)))
+    access_token = ''.join((random.choice(string.ascii_letters + string.digits) for _ in range(32)))
     response = {
         "access_token": access_token,
         "token_type": "bearer"
